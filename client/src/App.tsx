8cee19422f7766e5845bd055540170f9ec737e25
yeji424 <yejiborn424@gmail.com>
2025-06-16 16:23:18 +0900

    Feat: 전체 레이아웃 설정
    - Tailwind 형식으로 공통 화면 레이아웃 설정
    - App.tsx에 테스트 div가 있음(예시용)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,18 @@ import PlanChatTester from './components/ChatTester';
 function App() {
   return (
     <div>
+      <div className="bg-blue-100">
+      <h1 className="text-center text-gray700 mb-8">
+        Tailwind CSS 테스트 중입니다
+      </h1>
+      <p className="text-lg text-gray-400 mb-4">
+        이 문장은 Tailwind의  기본 텍스트 스타일이 적용돼야 합니다.
+      </p>
+      <button className="px-4 py-2 bg-primary-pink-40 text-white rounded hover:bg-primary-pink transition">
+        눌러보세요
+      </button>
+      </div>
+      {/* PlanChatTester 컴포넌트 렌더링 */}
       <PlanChatTester />
     </div>
   );
