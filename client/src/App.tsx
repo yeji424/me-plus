417f9267a1e7f46529257c785265c30fb5846337
leeemingyu <skystar9927@naver.com>
2025-06-16 19:49:43 +0900

    fix: 라우트 연결 페이지 수정
    '/' 👉 MainPage 컴포넌트
    '/chat-test' 👉 ChatTestPage 컴포넌트

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,17 @@ import ChatTestPage from './pages/ChatTestPage';
 import ChatbotPage from './pages/ChatbotPage';
 import PlanTestPage from './pages/PlanTestPage';
 import ComparePage from './pages/ComparePage';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ChatTestPage />} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/chatbot" element={<ChatbotPage />} />
         <Route path="/plan-test" element={<PlanTestPage />} />
         <Route path="/compare" element={<ComparePage />} />
+        <Route path="/chat-test" element={<ChatTestPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
