7283b5fa15c20d3b71a074ca59151945914e12bf
Yongyeon Kim <seyeon.dev@gmail.com>
2025-06-17 09:09:27 +0900

    chore: 기본 tool 내용 정의

--- server/services/gptFuncCallTest.js
+++ server/services/gptFuncCallTest.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import { OpenAI } from 'openai';
+import { BASE_PLAN_TOOL_DEF } from '../utils/gptTools.js';
 
 dotenv.config();
 
@@ -7,57 +8,9 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 const tools = [
   {
-    strict: true,
-    type: 'function',
+    ...BASE_PLAN_TOOL_DEF,
     name: 'get_plans',
     description: '요금제 전체 목록 조회',
-    parameters: {
-      type: 'object',
-      additionalProperties: false,
-      properties: {
-        plans: {
-          type: 'array',
-          items: {
-            type: 'object',
-            additionalProperties: false,
-            properties: {
-              _id: { type: 'string' },
-              name: { type: 'string' },
-              description: { type: 'string' },
-              dataGb: {
-                type: 'number',
-                description: '기본 데이터',
-              },
-              sharedDataGb: { type: 'number', description: '공유 데이터' },
-              monthlyFee: { type: 'number', description: '월정액' },
-              detailUrl: { type: 'string', description: '상세페이지 링크' },
-              bundleBenefit: {
-                type: 'object',
-                description: '결합 혜택 정보',
-                additionalProperties: false,
-                properties: {
-                  _id: { type: 'string' },
-                  name: { type: 'string' },
-                  description: { type: 'string' },
-                },
-                required: ['_id', 'name', 'description'],
-              },
-            },
-            required: [
-              '_id',
-              'name',
-              'description',
-              'dataGb',
-              'sharedDataGb',
-              'monthlyFee',
-              'detailUrl',
-              'bundleBenefit',
-            ],
-          },
-        },
-      },
-      required: ['plans'],
-    },
   },
 ];
 
