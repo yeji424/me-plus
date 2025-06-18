fcc3c6bdc04757a02e7326bac682777d954f01d2
chemnida <chaemin9073@gmail.com>
2025-06-18 17:40:03 +0900

    Chore : Lint에러 해결
    - 사용하지 않는 import주석처리

--- client/src/pages/TestResultPage.tsx
+++ client/src/pages/TestResultPage.tsx
@@ -1,9 +1,7 @@
 // import React from 'react'
 
-// const TestResultPage = () => {
-//   return (
-//     <div>TestResultPage</div>
-//   )
-// }
+const TestResultPage = () => {
+  return <div>TestResultPage</div>;
+};
 
-// export default TestResultPage
+export default TestResultPage;
