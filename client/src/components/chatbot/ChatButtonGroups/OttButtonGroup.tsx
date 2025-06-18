5eb2372a5502efaeec28fad501614946ba9a27c0
hyonun321 <hyo_nun@kakao.com>
2025-06-18 21:58:31 +0900

    Refactor: OTT_SERVICES를 주입받아 렌더링 하도록 수정
    - 별건아니고, 추후 OTT서비스가 늘어날 수도 있으니 저렇게 리스트로 해두면 나중에 서버에서 받아서 렌더링할 수도 있는 형태가 됩니다.
    - 확장성을 고려한다고 보시면 될 것 같아요

--- client/src/components/chatbot/ChatButtonGroups/OttButtonGroup.tsx
+++ client/src/components/chatbot/ChatButtonGroups/OttButtonGroup.tsx
@@ -5,19 +5,29 @@ import WatchaIcon from '@/assets/icon/watcha.png';
 import DisneyIcon from '@/assets/icon/diseny.png';
 import DraggableScroll from '@/components/common/DraggableScroll';
 
+interface OttService {
+  id: string;
+  label: string;
+  icon: string;
+}
+
+const OTT_SERVICES: OttService[] = [
+  { id: 'netflix', label: '넷플릭스', icon: NetflixIcon },
+  { id: 'tving', label: '티빙', icon: TvingIcon },
+  { id: 'watcha', label: '왓챠', icon: WatchaIcon },
+  { id: 'disney', label: '디즈니 +', icon: DisneyIcon },
+];
+
 const OttButtonGroup = () => {
   return (
     <DraggableScroll className="flex flex-nowrap gap-1 hide-scrollbar overflow-visible">
-      <ChatButton
-        label="넷플릭스"
-        icon={<img src={NetflixIcon} alt="넷플릭스" />}
-      />
-      <ChatButton label="티빙" icon={<img src={TvingIcon} alt="티빙" />} />
-      <ChatButton label="왓챠" icon={<img src={WatchaIcon} alt="왓챠" />} />
-      <ChatButton
-        label="디즈니 +"
-        icon={<img src={DisneyIcon} alt="디즈니+" />}
-      />
+      {OTT_SERVICES.map((service) => (
+        <ChatButton
+          key={service.id}
+          label={service.label}
+          icon={<img src={service.icon} alt={service.label} />}
+        />
+      ))}
     </DraggableScroll>
   );
 };
