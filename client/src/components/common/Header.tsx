140ae1ca8eb442d62494deba069f871097ba18c0
chemnida <chaemin9073@gmail.com>
2025-06-18 14:33:58 +0900

    Feat: Header gray700수정
    - Header 글씨 색상 변경 gray700

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -24,7 +24,7 @@ const Header = ({ title, onBackClick, iconButtons }: HeaderProps) => {
     <header className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-[600px] px-5 pt-5 pb-3 bg-transparent flex items-center justify-between z-50">
       <BackIcon onClick={backPage} className="icon-button" />
       {title && (
-        <p className=" absolute left-1/2 -translate-x-1/2 text-base text-secondary-purple-80">
+        <p className=" absolute left-1/2 -translate-x-1/2 text-base text-gray700">
           {title}
         </p>
       )}
