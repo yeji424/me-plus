c8058a19f983c52216955d817efbb8f3508456be
chemnida <chaemin9073@gmail.com>
2025-06-26 11:17:01 +0900

    Feat : 요금제자세히보기 모달추가
    - 외부링크로 넘어가는건 불친절하다
    - 모달 띄우기
    - 정말 넘어가시겠습니까?

--- client/src/components/common/Modal.tsx
+++ client/src/components/common/Modal.tsx
@@ -20,7 +20,7 @@ const Modal: React.FC<ModalProps> = ({
     if (isOpen) {
       document.body.style.overflow = 'hidden';
     } else {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = 'auto';
     }
 
     const handleEsc = (event: KeyboardEvent) => {
